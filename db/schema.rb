e9eb19879e8d691e0fef6f13b26ddddbdffe167d
Jacob Hampton <jhampton792@gmail.com>
2021-02-09 10:36:50 -0600

    Added associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725185638) do
+ActiveRecord::Schema.define(version: 20210209163257) do
+
+  create_table "heroine_powers", force: :cascade do |t|
+    t.integer "heroine_id"
+    t.integer "power_id"
+    t.string "strength"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "heroines", force: :cascade do |t|
     t.string "name"
