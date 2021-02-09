e5c782a31d68bed5460086f24c154ef9ea53e353
Jacob Hampton <jhampton792@gmail.com>
2021-02-09 11:15:43 -0600

    Added basic validation for heroine_power instances

--- app/models/heroine_power.rb
+++ app/models/heroine_power.rb
@@ -1,4 +1,8 @@
 class HeroinePower < ApplicationRecord
     belongs_to :heroine
     belongs_to :power
+
+    validates :heroine_id, presence: true
+    validates :power_id, presence: true
+    validates :strength, inclusion: { in: ["Weak", "Average", "Strong"] }
 end
